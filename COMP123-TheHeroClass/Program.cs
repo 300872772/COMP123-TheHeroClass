dfa3d96884b4b8108c980a310397242b990caf2b
300872772 <mamun_131@hotmail.com>
2016-06-01 00:58:19 -0400

    Hero class has been made with all instance variable, properties, private and public methods

--- COMP123-TheHeroClass/Program.cs
+++ COMP123-TheHeroClass/Program.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ *Author: Md Mamunur Rahman
+ * Student ID: 300872772 
+ *
+ * Date: May 13, 2016
+ * Description: This program demonstrates a game charachtar named 'Hero', which has Name, Strength, Speed 
+ * and Health properties to be shown to Console. 
+ * 
+ * 
+ * Version: 0.0.1 - Hero class has been made with all instance variable, properties, private and public methods 
+ */
 namespace COMP123_TheHeroClass
 {
     class Program
