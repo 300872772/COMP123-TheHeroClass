8ca2c65eac6f7c97d19ed800385017d758f04603
300872772 <mamun_131@hotmail.com>
2016-06-01 00:46:49 -0400

    added all properties, private and public methods as per asssignment given

--- COMP123-TheHeroClass/Hero.cs
+++ COMP123-TheHeroClass/Hero.cs
@@ -9,8 +9,13 @@ namespace COMP123_TheHeroClass
 
     /**
      * <summary>
+     * This class defines a charecter Hero which has 4 fields, 1 properties, 3 private methods and 2 public methods 
      * 
-     * 
+     * @class Hero
+     * @field {int} _strength
+     * @field {int} _speed
+     * @field {int} _health
+     * @field {string} _name
      * </summary>
      * 
      */
@@ -26,28 +31,165 @@ namespace COMP123_TheHeroClass
         //PUBLIC PROPERTIES+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         /**
         * <summary>
+        * This is a property for _name field
+        * @property {string} Name 
+        * </summary>
         * 
+        */
+
+        public string Name
+        {
+
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                this._name = value;
+            }
+        }
+        /**
+        * <summary>
+        * This is a property for _strength field
+        * @property {int} Strength 
+        * </summary>
+        * 
+        */
+
+        public int Strength
+        {
+
+            get
+            {
+                return this._strength;
+            }
+            set
+            {
+                this._strength = value;
+            }
+        }
+        /**
+        * <summary>
+        * This is a property for _speed field
+        * @property {int} Speed 
+        * </summary>
+        * 
+        */
+
+        public int Speed
+        {
+
+            get
+            {
+                return this._speed;
+            }
+            set
+            {
+                this._speed = value;
+            }
+        }
+        
+        /**
+        * <summary>
+        * This is a property for _health field
+        * @property {int} Health 
+        * </summary>
+        * 
+        */
+
+        public int Health
+        {
+
+            get
+            {
+                return this._health;
+            }
+            set
+            {
+                this._health = value;
+            }
+        }
+
+
+
+        //CONSTRUCTOR+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        /**
+        * <summary>
+        * This is the constructor that takes the name parametere and passes to nameinstance variable
+        *  and call _generateAbilit Method.
+        * @param {string} name 
         * 
         * </summary>
         * 
         */
 
-        //CONSTRUCTOR+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-        /**
-        * <summary>
-        * 
-        * 
-        * </summary>
-        * 
-        */
+        public Hero(string name)
+        {
+            _generateAbility();
+            this.Name = name;
+
+
+        }
         //PRIVATE METHODES++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         /**
         * <summary>
+        * This the privte method that generate random nuber and passed to _speed, _strength and _health instance variable
         * 
-        * 
+        * @returns {void}        * 
         * </summary>
         * 
         */
+
+        private void _generateAbility ()
+        {
+            Random random = new Random();
+            this.Speed = random.Next(1, 100);
+            this.Strength = random.Next(1, 100);
+            this.Health = random.Next(1, 100);
+        }
+        
+         /**
+        * <summary>
+        * This the privte method that generate 
+        * 
+        * @param (int) setProb
+        * @returns {bool}        * 
+        * </summary>
+        * 
+        */
+
+        private bool _hitAttempt(int setProb)
+        {
+            Random random = new Random();
+            int probability = random.Next(100);
+
+            if(probability<=setProb)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+         /**
+        * <summary>
+        * This the privte method that generate 
+        * 
+        * @returns {int}        * 
+        * </summary>
+        * 
+        */
+        private int _hitDamage()
+        {
+            Random random = new Random();
+            int damage = (random.Next(1, 6))* this.Strength;
+
+            return damage;
+        }
 
         //PUBLIC METHODS++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         /**
@@ -57,7 +199,29 @@ namespace COMP123_TheHeroClass
         * </summary>
         * 
         */
+        public void Fight()
+        {
+            if (_hitAttempt(20))
+            {
+               Console.WriteLine( "Damage value is{0} ",_hitDamage());
 
+            }
+
+        }
+
+        /**
+        * <summary>
+        * 
+        * 
+        * </summary>
+        * 
+        */
+
+        public void Show()
+        {
+            Console.WriteLine("Name:{0}\n Strength: {1}\n Speed:{2}\n Health:{3}\n",this.Name, this.Strength, this.Speed, this.Health);
+
+        }
 
     }
 }
